1ac82f1d8243bbb18b8960b716fd239d5ba51a7d
Damian Mayer <damian.matias.mayer@hotmail.com>
2025-06-30 00:27:24 -0300

    haciendo comentarios, ya terrmine los js, css y html de contacto y energizante

--- js/contacto.js
+++ js/contacto.js
@@ -1,13 +1,23 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
+
+  //selecciona el formulario con clase .formulario-contacto
   const formulario = document.querySelector('.formulario-contacto');
+  //seleccion el contenedor donde se va a mostrar el mensaje de exito con id mensaje-exito
   const mensaje = document.getElementById('mensaje-exito');
 
+  //cuando el usuario hace clic en el boton "enviar"
   formulario.addEventListener('submit', function (e) {
+
     e.preventDefault();
 
+    //Cambia el texto del mensaje de éxito.
     mensaje.textContent = '¡Gracias! Tu mensaje fue enviado correctamente. Nos pondremos en contacto a la brevedad.';
     mensaje.style.display = 'block';
 
+    // Limpia todos los campos del formulario.
     formulario.reset();
   });
 });
---
+++ js/energizantes.js
@@ -0,0 +1,20 @@
+
+//Crea un "observer" que vigila elementos para ver si están dentro de la pantalla (al menos un 10%, definido por threshold: 0.1) para la clase de css "visible".
+const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add("visible");
+            }
+          });
+        },
+        { threshold: 0.1 }
+      );
+
+       //busca todos los elementos que tengan la clase "animar" 
+      document
+        .querySelectorAll(".animar")
+        .forEach((el) => observer.observe(el));
+
+
+        //esto se usa para aplicar animaciones o transicioes solo cuando los elemntos se vuelven visibles al hacer scroll.
