d568eeef4aa787632fc6719d9cc7b692e7440f39
Damian Mayer <damian.matias.mayer@gmail.com>
2025-09-06 12:56:54 -0300

    agregando comentarios

--- js/gsap-registro.js
+++ js/gsap-registro.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // animar el contenedor del formulario
-  gsap.from(".registro-box", {
+  gsap.from(".registro-box", { //Se usa para animar desde un estado inicial hacia el estado natural del elemento.
     duration: 1,           // duración de la animación (1s)
     opacity: 0,            // empieza invisible
     y: 50,                 // empieza 50px más abajo
